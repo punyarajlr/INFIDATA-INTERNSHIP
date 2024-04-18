7e250f66f8e4dec8b3093cff3dcd782cf1305fcf
punya <rpunya8@gmail.com>
2024-04-18 12:09:00 +0530

    added matplotlib

---
+++ machine learning/matplotlib/2_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=np.array([0,6])
+y=np.array([0,10])
+
+plt.plot(x,y,'-o') #creating a plot
+plt.show() #displaying the plot
+# *
+# o
+# -
+# --
